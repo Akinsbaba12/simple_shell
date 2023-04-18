e183d042e2372f24ac33af8cb80a79b234529fc5
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-18 05:31:45 -0700

    updated header files

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int _strlen(char *s);
+
+void execmnd(char **argv);
+
+char *get_path_location(char *command);
+#endif
+
