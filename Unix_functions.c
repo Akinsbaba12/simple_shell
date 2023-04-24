f2ab53625435d81f34359ae3fb94fd7c654b9b90
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-23 17:38:40 -0700

    betty check done

--- Unix_functions.c
+++ Unix_functions.c
@@ -1,94 +1,95 @@
 #include "shell.h"
 
 /**
- * *_strcat - concatenates @src to @dest
- * @src: the source string to append to @dest
- * @dest: the destiation string to be concatenated upon
- * Return:pointer to the resulting string
- */
+* _strcat - concatenates @src to @dest
+* @src: the source string to append to @dest
+* @dest: the destiation string to be concatenated upon
+* Return:pointer to the resulting string
+*/
 
 char *_strcat(char *dest, char *src)
-{
-        int index = 0;
-        int dest_len = 0;
 
-        while (dest[index++])
-                dest_len++;
-        for (index = 0; src[index]; index++)
-                dest[dest_len++] = src[index];
-        return (dest);
+{
+int index = 0, dest_len = 0;
+
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
 
 /**
- * _strdup - function copys string to new memory space location
- * @str: char string to copy
- * Return: 0
- */
+* _strdup - function copys string to new memory space location
+* @str: char string to copy
+* Return: 0
+*/
 
 char *_strdup(char *str)
 
 {
 
-        char *ptr;
-        int a, ret = 0;
+char *ptr;
+int a, ret = 0;
 
-        if (str == NULL)
+if (str == NULL)
 
-                return (NULL);
+return (NULL);
 
-        a = 0;
+a = 0;
 
-        while (str[a] != '\0')
+while (str[a] != '\0')
 
-                a++;
+a++;
 
-        ptr = malloc(sizeof(char) * (a + 1));
+ptr = malloc(sizeof(char) * (a + 1));
 
-        if (ptr == NULL)
+if (ptr == NULL)
 
-                return (NULL);
+return (NULL);
 
-        for (ret = 0; str[ret]; ret++)
+for (ret = 0; str[ret]; ret++)
 
-                ptr[ret] = str[ret];
+ptr[ret] = str[ret];
 
-        return (ptr);
+return (ptr);
 
 }
 
 /**
- * _strlen - checks the length of a string
- * @s: string to check
- *
- * Return: int length of the string
- */
+* _strlen - checks the length of a string
+* @s: string to check
+* Return: int length of the string
+*/
 
 int _strlen(char *s)
-{
-        int length = 0;
 
-        while (s[length])
-        length++;
-        return (length);
+{
+int length = 0;
+
+while (s[length])
+length++;
+return (length);
 }
 
 /**
- * *_strcpy - function copies the string pointed to by src
- * @dest: char type string
- * @src: char type string
- * Description: Copy the string pointed to by pointer __src__ to
- * the buffer pointed to by **dest**
- * Return: Pointer to _dest_
- */
+* _strcpy - function copies the string pointed to by src
+* @dest: char type string
+* @src: char type string
+* Description: Copy the string pointed to by pointer __src__ to
+* the buffer pointed to by **dest**
+* Return: Pointer to _dest_
+*/
 
 char *_strcpy(char *dest, char *src)
+
 {
-        int a = -1;
+int a = -1;
 
-        do {
-                a++;
-                dest[a] = src[a];
-        } while (src[a] != '\0');
+do {
+a++;
+dest[a] = src[a];
+} while (src[a] != '\0');
 
-        return (dest);
+return (dest);
 }
