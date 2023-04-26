5d4f8147e501e8de3bea5e93ffcc6a5c13f42582
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-26 06:01:55 -0700

    more user defined function file and betty check

---
+++ Unix_functions3.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * _getenv - function returns an environment variable
+ * @name: poinetr to character variable
+ * Return: path of environment variable
+ * Else: NULL if variable does not exist
+ */
+
+char *_getenv(char *name)
+{
+  char **environ = NULL;
+    char **env;
+
+	int name_len = _strlen(name);
+
+	for (env = environ; env != NULL; env++)
+	{
+		if (_strncmp(name, *env, name_len) == 0 && (*env)[name_len] == '=')
+		{
+			return (&((*env)[name_len + 1]));
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * _strncmp - function compares the first @num character
+ * of @str1 and @str2
+ * @str1: pointer to first string character
+ * @str2: pointer to second string character
+ * @num: number of bytes to compare
+ * Return: 1 - if @str1 > @str2
+ * Else: -1
+ */
+
+int _strncmp(const char *str1, const char *str2, size_t num)
+{
+	size_t i;
+
+	for (i = 0; i < num; ++i)
+	{
+		if (str1[i] != str2[i])
+		{
+			return ((str1[i] < str2[i]) ? -1 : 1);
+		}
+		else if (str1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
