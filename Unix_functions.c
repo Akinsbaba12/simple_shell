2b862ab6f60f7e79d6497fdb16b1db35601e69a7
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-26 03:14:53 -0700

    betty check done

--- Unix_functions.c
+++ Unix_functions.c
@@ -1,120 +1,120 @@
 #include "shell.h"
 
 /**
-* *_strcat - concatenates @src to @dest
-* @src: the source string to append to @dest
-* @dest: the destiation string to be concatenated upon
-* Return:pointer to the resulting string
-*/
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
+ */
 
 char *_strcat(char *dest, char *src)
 
 {
-int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
 
 /**
-* *_strdup - function copys string to new memory space location
-* @str: char string to copy
-* Return: 0
-*/
+ * *_strdup - function copys string to new memory space location
+ * @str: char string to copy
+ * Return: 0
+ */
 
 char *_strdup(char *str)
 
 {
 
-char *ptr;
-int a, ret = 0;
+	char *ptr;
+	int a, ret = 0;
 
-if (str == NULL)
+	if (str == NULL)
 
-return (NULL);
+		return (NULL);
 
-a = 0;
+	a = 0;
 
-while (str[a] != '\0')
+	while (str[a] != '\0')
 
-a++;
+		a++;
 
-ptr = malloc(sizeof(char) * (a + 1));
+	ptr = malloc(sizeof(char) * (a + 1));
 
-if (ptr == NULL)
+	if (ptr == NULL)
 
-return (NULL);
+		return (NULL);
 
-for (ret = 0; str[ret]; ret++)
+	for (ret = 0; str[ret]; ret++)
 
-ptr[ret] = str[ret];
+		ptr[ret] = str[ret];
 
-return (ptr);
+	return (ptr);
 
 }
 
 /**
-* _strlen - checks the length of a string
-* @s: string to check
-* Return: int length of the string
-*/
+ * _strlen - checks the length of a string
+ * @s: string to check
+ * Return: int length of the string
+ */
 
 int _strlen(char *s)
 
 {
-int length = 0;
+	int length = 0;
 
-while (s[length])
-length++;
-return (length);
+	while (s[length])
+		length++;
+	return (length);
 }
 
 /**
-* *_strcpy - function copies the string pointed to by src
-* @dest: char type string
-* @src: char type string
-* Description: Copy the string pointed to by pointer __src__ to
-* the buffer pointed to by **dest**
-* Return: Pointer to _dest_
-*/
+ * *_strcpy - function copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer __src__ to
+ * the buffer pointed to by **dest**
+ * Return: Pointer to _dest_
+ */
 
 char *_strcpy(char *dest, char *src)
 
 {
-int a = -1;
+	int a = -1;
 
-do {
-a++;
-dest[a] = src[a];
-} while (src[a] != '\0');
+	do {
+		a++;
+		dest[a] = src[a];
+	} while (src[a] != '\0');
 
-return (dest);
+	return (dest);
 }
 
 /**
-* *_strchr - function searches a string for an integer  
-* @str: pointer to string given
-* @ch: integer value to search for
-* Return: if success - pointer to the ineteger character
-* Else: NULL
-*/
+ * *_strchr - function searches a string for an integer
+ * @str: pointer to string given
+ * @ch: integer value to search for
+ * Return: if success - pointer to the ineteger character
+ * Else: NULL
+ */
 
 char *_strchr(const char *str, int ch)
 {
-    while (*str != '\0')
-      {
-        if (*str == ch)
-	  {
-            return (char *) str;
-        }
-        str++;
-    }
-    if (ch == '\0')
-      {
-        return (char *) str;
-    }
-    return (NULL);
+	while (*str != '\0')
+	{
+		if (*str == ch)
+		{
+			return ((char *) (str));
+		}
+		str++;
+	}
+	if (ch == '\0')
+	{
+		return ((char *) (str));
+	}
+	return (NULL);
 }
--- get_path_location.c
+++ get_path_location.c
@@ -1,63 +1,51 @@
 #include "shell.h"
 
+/**
+* *get_path_location - function gets the full path location of a command.
+* @command: character command to find.
+* Return: path location
+* Else: NULL - if the path was not found.
+*/
+
 char *get_path_location(char *command)
 {
-  char *pth, *pth_cp, *pth_tok, *file_pth;
-  int cmd_len, dir_len;
-  struct stat buff;
+	char *pth, *pth_cp, *pth_tok, *file_pth;
+	int cmd_len, dir_len;
+	struct stat buff;
 
-
-  pth = getenv("PATH");
-
-  if (pth)
-    {
-      pth_cp = _strdup(pth);
-
-      cmd_len = _strlen(command);
-
-      pth_tok = strtok(pth_cp, ":");
-
-      while (pth_tok != NULL)
+	pth = getenv("PATH");
+	if (pth)
 	{
-	  dir_len = _strlen(pth_tok);
+		pth_cp = _strdup(pth);
+		cmd_len = _strlen(command);
+		pth_tok = strtok(pth_cp, ":");
 
-	  file_pth = malloc(cmd_len + dir_len + 2);
+		while (pth_tok != NULL)
+		{
+			dir_len = _strlen(pth_tok);
+			file_pth = malloc(cmd_len + dir_len + 2);
+			_strcpy(file_pth, pth_tok);
+			_strcat(file_pth, "/");
+			_strcat(file_pth, command);
+			_strcat(file_pth, "\0");
 
-	  _strcpy(file_pth, pth_tok);
-
-	  _strcat(file_pth, "/");
-
-	  _strcat(file_pth, command);
-
-	  _strcat(file_pth, "\0");
-
-	  if (stat(file_pth, &buff) == 0)
-	    {
-	      free(pth_cp);
-
-	      return (file_pth);
-	    }
-	    else
-	    {
-	     free(file_pth);
-
-	     pth_tok = strtok(NULL, ":");
-
-	    }
-
-	    }
-
-	  free(pth_cp);
-
-	  if (stat(command, &buff) == 0)
-	    {
-	      return (command);
-
-	    }
-
-	    return (NULL);
-
-	    }
-
-	  return (NULL);
+			if (stat(file_pth, &buff) == 0)
+			{
+				free(pth_cp);
+				return (file_pth);
+			}
+			else
+			{
+				free(file_pth);
+			pth_tok = strtok(NULL, ":");
+			}
+		}
+		free(pth_cp);
+		if (stat(command, &buff) == 0)
+		{
+			return (command);
+		}
+		return (NULL);
+	}
+	return (NULL);
 }
