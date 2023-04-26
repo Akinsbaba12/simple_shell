30acd50298ca6c510626ccf9c358a09f1c1065b9
omotomiwa26 <omotomiwa26@gmail.com>
2023-04-26 05:57:48 -0700

    Unix CLI prompt display

---
+++ dis_prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * display_prompt - function displays a prompt for user command
+ * Return: void
+ */
+void display_prompt(void)
+{
+	char *dis_prompt = "(?) ";
+
+	write(STDERR_FILENO, dis_prompt, _strlen(dis_prompt));
+}
